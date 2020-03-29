0fee7d04e842266ff5037cd8f6a6c49a610fac93
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-29 15:53:17 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,9 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[5.2] 
+  def change 
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location 
+    end
+  end
 end
