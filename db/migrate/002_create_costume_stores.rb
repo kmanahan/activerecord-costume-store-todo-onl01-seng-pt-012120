b3b9c832775729f0fb9bd065d8e66b2096654968
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-29 15:32:39 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,7 @@
     create_table :costume_stores do |t|
       t.string :name 
       t.string :location 
-      t.string :
+      t.string :num_of_employees
       t.string :image_url
     end
   end 
