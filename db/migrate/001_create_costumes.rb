05a5b3077f506ce4f1e375d7506cab45863b0769
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-29 15:56:26 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
       t.string :name 
       t.integer :price 
       t.string :size 
-      t.url :image_url
+      t.string :image_url
       t.timestamp :created_at 
       t.timestamp :updated_at
     end
