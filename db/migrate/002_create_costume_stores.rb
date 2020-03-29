6fc75e6e03a198a535b50f7dcbd533d577052f36
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-29 15:31:42 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,10 @@
-# Create your costume_stores migration here
+# Create your costume_stores migration hereclass CreateCostumes < ActiveRecord::Migration[5.2]
+  def change 
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location 
+      t.string :size 
+      t.string :image_url
+    end
+  end 
+end
