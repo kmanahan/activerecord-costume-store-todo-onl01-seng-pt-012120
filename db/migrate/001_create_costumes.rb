3d6b7f63ada2eef811a74c73349cd601112458ca
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-29 15:24:41 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,6 +12,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
     create_table :costumes do |t|
       t.string :name 
       t.integer :price 
+      t.string :size 
       t.
   end 
 end 
