76938438e8cbbef6bf0993838edc9cac10288107
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-29 16:01:59 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
     create_table :costume_stores do |t|
       t.string :name 
       t.string :location 
-      t.string :num_of_employees
+      t.integer :num_of_employees
       t.string :image_url
       t.boolean :still_in_business
       t.datetime :opening_time
